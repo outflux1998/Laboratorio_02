11109c1ede6e388750be986a393d9aadb947646f
caio-magnani <49353875+caio-magnani@users.noreply.github.com>
2022-03-20 18:42:09 -0300

    Create AluguelDeAutomoveisApplication.java

---
+++ Implementação/aluguel-de-automoveis/src/main/java/com/test/alugueldeautomoveis/AluguelDeAutomoveisApplication.java
@@ -0,0 +1,13 @@
+package com.test.alugueldeautomoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguelDeAutomoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluguelDeAutomoveisApplication.class, args);
+	}
+
+}
