b299ed9eebf6cdd33ab183356ae11fe2f807bc99
caio-magnani <49353875+caio-magnani@users.noreply.github.com>
2022-03-20 18:40:49 -0300

    C0, U0 - adiciona AddressController

---
+++ Implementação/aluguel-de-automoveis/src/main/java/com/test/alugueldeautomoveis/controller/client/AddressController.java
@@ -0,0 +1,37 @@
+package com.test.alugueldeautomoveis.controller.client;
+import java.util.List;
+
+import com.test.alugueldeautomoveis.model.client.Address;
+import com.test.alugueldeautomoveis.repository.AddressRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/clients/address")
+public class AddressController {
+
+	@Autowired
+	private AddressRepository repository;
+	
+	@GetMapping
+	public List<Address> getAll() {
+		return repository.findAll();
+	}
+
+	@GetMapping(value = "/{id}")
+	public Address get(@PathVariable Long id) {
+		return repository.getById(id);
+	}
+	
+	@PostMapping
+	public Address insert(@RequestBody Address address) {
+		return repository.save(address);
+	}
+}
