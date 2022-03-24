4a8ea519bb13748ac135d91c0035a4669f063d46
caio-magnani <49353875+caio-magnani@users.noreply.github.com>
2022-03-24 20:01:23 -0300

    try again with mvn

--- Implementação/aluguel-de-automoveis/src/main/java/com/lab/alugueldeautomoveis/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lab.alugueldeautomoveis.repository;
-
-import com.lab.alugueldeautomoveis.model.client.Address;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long>{
-
-}
